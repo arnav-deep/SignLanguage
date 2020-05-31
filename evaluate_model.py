2e7320c6d93c8a125f9b2cbcb1828b1249d730aa
arnav-deep <arnavd4@gmail.com>
2020-05-31 18:51:27 +0530

    init commit

---
+++ evaluate_model.py
@@ -0,0 +1,22 @@
+import keras
+import pandas as pd
+from keras.models import load_model
+from sklearn.preprocessing import LabelBinarizer
+
+model = load_model('Models/base_model_asl_50')
+
+test_df = pd.read_csv("MNSIT/sign_mnist_validate/sign_mnist_validate.csv")
+
+# print(test_df)
+
+y_test = test_df['label']
+del test_df['label']
+
+x_test = test_df.values
+x_test = x_test / 255
+x_test = x_test.reshape(-1, 28, 28, 1)
+
+label_binarizer = LabelBinarizer()
+y_test = label_binarizer.fit_transform(y_test)
+
+print('Accuracy of the model is - ', model.evaluate(x_test, y_test)[1] * 100, "%")
---
+++ test_live_cam.py
@@ -0,0 +1,4 @@
+from keras.models import load_model
+import cv2
+
+capture = cv2.VideoCapture(0)
